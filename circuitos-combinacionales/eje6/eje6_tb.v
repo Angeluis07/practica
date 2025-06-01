5ef0eba7e08316e6fc2ab605cb3d4a40876e4558
Palacios Angel Luis <angeluis.0711@gmail.com>
2025-05-31 23:22:42 -0300

    Agrego testbench de ejercicio 6

--- circuitos-combinacionales/eje6/eje6_tb.v
+++ circuitos-combinacionales/eje6/eje6_tb.v
@@ -3,19 +3,25 @@
 module eje6_tb();
 
 // Entradas
-
+reg [11:0] codigoH_recibido;
 // Salidas
+wire [7:0] datosCorregidos;
 
-//eje4 UUT(
-   
-//);
-
+eje6_cor correcion (
+    .codigoH_recibido(codigoH_recibido),
+    .datosCorregidos(datosCorregidos)
+);
 
 
 initial begin
   $dumpfile("eje6_tb.vcd");
   $dumpvars(0, eje6_tb);
-
+  // Valor esperado en datoCorregidos es 00100110
+  // Test 1: Sin errores
+  //codigoH_recibido = 12'b110101000110;
+  // Cambiamos un bit para simular un error
+  codigoH_recibido = 12'b110101000111;
+  #10; // Esperamos un tiempo para ver el resultado
   $finish;
 end
 
