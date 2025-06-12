f837bcc040404303eca36cf47324835ba7da417d
Palacios Angel Luis <angeluis.0711@gmail.com>
2025-06-12 20:48:27 -0300

    Agrego eje5 y eje1

--- circuitos-aritmeticos-logicos/eje5/eje5_tb.v
+++ circuitos-aritmeticos-logicos/eje5/eje5_tb.v
@@ -3,15 +3,39 @@
 
 module eje5_tb();
 
-reg [3:O] A;
+reg [3:0] A;
 reg [2:0] op;
-wire [3:0] X;
+wire [3:0] datosMod;
 
 eje5 UUT(
-    .A(A),
+    .datos(A),
     .op(op),
-    .X(X);
-
+    .datosMod(datosMod)
 );
+
+
+initial begin
+    $dumpfile("eje5_tb.vcd");
+    $dumpvars(0, eje5_tb);
+    A = 4'b0111;
+    op = 3'b000;
+    #10;
+    op = 3'b001;
+    #10;
+    op = 3'b010;
+    #10;
+    op = 3'b101;
+    #10;
+    op = 3'b110;
+    #10;
+    op = 3'b011;
+    #10;
+    op = 3'b111;
+    #10;
+    op = 3'b100;
+    #10;
+    
+    $finish;
+end
 
 endmodule
